8d06698995561767d4c89b7dae3376742f6e5c4a
Reginald86 <49849893+wgreune@users.noreply.github.com>
2024-03-29 18:34:25 -0500

    Added grid range, color/black button

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Etch-A-Sketch
+
 The Odin Project:  https://www.theodinproject.com/lessons/foundations-etch-a-sketch
+
+You can toggle back-and-forth between black and colored pixels.  Eventlistener is mouseover.
+
+Use the range button to change grid size from 4x4 to 96x96.
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-
-let black = document.querySelector("#black");
-let color = document.querySelector("#color");
+const blackBtn = document.querySelector("#black");
+const colorBtn = document.querySelector("#color");
 let numGrid = document.getElementById("gridValue");
 let pad = document.getElementById("etch");
 const GRID = document.querySelector("#gridInput");
-
+let useRandomColor = false;
+let useBlack = false;
 
 
 
@@ -18,27 +18,64 @@ GRID.addEventListener("input", (event) => {
 
 
 
-function board (num) {
-    for(let i = 0; i < num; i++) {
-        let row = document.createElement("div");
-        row.classList.add("row");
-        pad.appendChild(row);
+function board(num) {
+  for (let i = 0; i < num; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    pad.appendChild(row);
 
-    for(let j = 0; j < num; j++) {
-        let column = document.createElement("div");
-        column.classList.add("column");
-        row.appendChild(column);
-      }
-
+    for (let j = 0; j < num; j++) {
+      let column = document.createElement("div");
+      column.classList.add("column");
+      row.appendChild(column);
+      column.addEventListener('mouseover', (event) => {
+        if (useRandomColor) {
+          randomColor(event);
+          
+        }
+        if (useBlack) {
+          blackColor(event);
+        }
+      });
     }
- }
+  }
+}
 
  function clearPad() {
   pad.textContent = '';
 }
 
+function random(number){
+  return Math.floor(Math.random() * (number + 1));
+}
+
+function randomColor(event){
+  const rainbow = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  event.target.style.background = rainbow;
+}
+
+
+function blackColor(event){
+  event.target.style.background = "black";
+}
+
+
+colorBtn.addEventListener('click', () => {
+  useRandomColor = !useRandomColor; 
+  useBlack = false;
+});
+
+
+blackBtn.addEventListener('click', () => {
+  useBlack = !useBlack; 
+  useRandomColor = false;
+});
+
 
 board(GRID.value);
+
+
+
 
 
 
